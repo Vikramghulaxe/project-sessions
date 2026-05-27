e200f0a9ea120338ab417f293709e6601fc0fcea
Vikramghulaxe <vikram.ghulaxe@gmail.com>
2026-05-27 10:53:58 +0530

    JWT Auth Added

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer
+)
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
