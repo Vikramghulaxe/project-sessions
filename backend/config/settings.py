e5c640427de7fb337aad973412c8655305a2f7f9
Vikramghulaxe <vikram.ghulaxe@gmail.com>
2026-05-28 13:41:40 +0530

    Ratelimiter added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,4 +137,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "100/day",
+    }
 }
